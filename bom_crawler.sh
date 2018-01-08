73af332bb51f8e601789a4a887c6b0b2b0c10d37
Connor McLeod <z5058240@ad.unsw.edu.au>
2018-01-09 10:19:01 +1100

    Remove unecessary files

--- bom_crawler.sh
+++ bom_crawler.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-adelaide=`wget -q -O- "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-106946031&p_stn_num=023123" | egrep "<tr><th scope=" | tr '\d|<|>' ','`
+adelaide=`wget -q -O- "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-106946031&p_stn_num=023123" | egrep "<tr><th scope=" | tr '\D' ',' | tr '<|>|/' ' ' | tr 'td' ' '`
 # brisbane = "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-334786119&p_stn_num=040913"
 # sydney = "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-871369812&p_stn_num=066006"
 # melbourne = "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-1487202970&p_stn_num=086232"
@@ -10,3 +10,5 @@ adelaide=`wget -q -O- "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccOb
 # darwin = 
 
 echo "$adelaide"
+
+
