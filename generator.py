64998da68cb153cc2bbf8abfcd2a76fa8ecdd041
Connor McLeod <z5058240@ad.unsw.edu.au>
2018-01-08 11:17:27 +1100

    Rough working ready for simple demo

--- generator.py
+++ generator.py
@@ -25,15 +25,18 @@ report_period = get_all_daymonths()
 
 
 sql_daily_query = """
-SELECT distinct(o.smi), d.ref_no, d.ECS, d.installer, d.PVsize, d.panel_brand, d.address, d.state, d.site_status, d.install_date, d.supply_date, d.export_control, f.jan/31, f.feb/28, f.mar/31, f.apr/30, f.may/31, f.jun/30, f.jul/31, f.aug/31, f.sep/30, f.oct/31, f.nov/30, f.dec/31, o.obs_day, o.obs_month, sum(o.value)
+SELECT distinct(o.smi), d.ref_no, d.ECS, d.installer, d.PVsize, d.panel_brand, d.address, d.state, d.site_status, d.install_date, d.supply_date, d.export_control,
+		f.jan/31 as jan_daily, f.feb/28 as feb_daily, f.mar/31 as mar_daily, f.apr/30 as apr_daily, f.may/31 as may_daily, f.jun/30 as jun_daily, f.jul/31 as jul_daily, f.aug/31 as aug_daily, f.sep/30 as sep_daily, f.oct/31 as oct_daily, f.nov/30 as nov_daily, f.dec/31 as dec_daily, 
+		o.obs_day, o.obs_month, sum(o.value) as kWh_Generated, sum(o.value)/(f.nov/30) as percentage
 from observation as o 
 	join SMI_details as d
 		on o.smi=d.smi
 	join forecast as f
 		on o.smi=f.smi
-where o.datatype="kWh Generation" and o.obs_day=%s
+where o.datatype="kWh Generation"
 group by o.smi, o.obs_day, o.obs_month
-""" % days[0]
+order by o.smi, o.obs_month
+"""
 
 SMIs = pd.read_sql(sql_daily_query, conn)
 
