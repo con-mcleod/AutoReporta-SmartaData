6555c7e1e2c975916e5377f746db5c2f6e4b91f7
Connor McLeod <z5058240@ad.unsw.edu.au>
2018-01-09 11:51:35 +1100

    Finalising this branch

--- bom_crawler.sh
+++ bom_crawler.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+adelaide_link="http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-106946061&p_stn_num=023123"
+melbourne_link="http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-1487202970&p_stn_num=086232"
+sydney_link="http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-871369812&p_stn_num=066006"
+brisbane_link="http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-334786119&p_stn_num=040913"
+townsville_link="http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-205323725&p_stn_num=032040"
 
-adelaide=`wget -q -O- "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-106946031&p_stn_num=023123" | egrep "<tr><th scope=" | tr '\D' ',' | tr '<|>|/' ' ' | tr 'td' ' '`
-# brisbane = "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-334786119&p_stn_num=040913"
-# sydney = "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-871369812&p_stn_num=066006"
-# melbourne = "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-1487202970&p_stn_num=086232"
-# townsville = "http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=193&p_display_type=dailyDataFile&p_startYear=2017&p_c=-205323725&p_stn_num=032040"
-# perth =
-# darwin = 
+
+adelaide=`wget -q -O- "$adelaide_link" | egrep "<tr><th scope=" | sed -e 's/<[^>]*>/ /g'`
+# brisbane =`wget -q -O- "$brisbane_link" | egrep "<tr><th scope=" | sed -e 's/<[^>]*>/ /g'`
+# sydney =`wget -q -O- "$sydney_link" | egrep "<tr><th scope=" | sed -e 's/<[^>]*>/ /g'`
+# melbourne =`wget -q -O- "$melbourne_link" | egrep "<tr><th scope=" | sed -e 's/<[^>]*>/ /g'`
+# townsville =`wget -q -O- "$townsville_link" | egrep "<tr><th scope=" | sed -e 's/<[^>]*>/ /g'`
+
 
 echo "$adelaide"
-
-
